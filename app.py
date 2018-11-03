335b0b6be080bf8c247cd3fb1326d495a047f468
Abhishyant Khare <Abhishyant Khare>
2018-11-03 13:56:56 -0700

    added webhook verification

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+
 
 app = Flask(__name__)
 
@@ -6,9 +8,30 @@ app = Flask(__name__)
 def index():
     return 'Hi woorld'
 
-@app.route('/fbwebhook')
+@app.route('/fbwebhook', methods=["GET","POST"])
 def webhook():
-    return 'Parsing webhooks!'
+    if request.method == "GET":
+        return get_webhook()
+    elif request.method == "POST":
+        return post_webhook()
+    return 'Unexpected response'
+
+def get_webhook():
+    VERIFY_TOKEN = "serg_squad"
+
+    mode = request.args.get('hub.mode')
+    token = request.args.get('hub.verify_token')
+    challenge = request.args.get('hub.challenge')
+
+    if mode == 'subscribe' and token == VERIFY_TOKEN:
+        return 'Webhook succeeded!'
+    else:
+        return 'Fail!', 403
+
+def post_webhook():
+    print(str(request.get_json()))
+    return 'POST'
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
