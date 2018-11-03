f1b747f72b0af36c3e33260bd97c1d5cff001b01
Abhishyant Khare <Abhishyant Khare>
2018-11-03 16:43:02 -0700

    Added sockets

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,10 @@
+
 window.extAsyncInit = function () {
     // the Messenger Extensions JS SDK is done loading 
     console.log("SDK done loading!")
     MessengerExtensions.getContext('348666849024201',
         function success(thread_context) {
-            $.post("https://catchupbot.com/testing", {'hi': 'hello'}, function(data, status){
-
-                console.log("sent")
-            })
+            socket.emit('fbdata_event', {'hi': 'hello'})
         },
         function error(err) {
             // error
